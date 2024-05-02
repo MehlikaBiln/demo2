0a3e6767e68bad4f274645a237ec042409e209b0
MehlkaBilen <99501031+MehlkaBilen@users.noreply.github.com>
2024-05-02 15:01:02 +0300

    added docker file

--- src/main/java/com/example/demo2/HomeController.java
+++ src/main/java/com/example/demo2/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
         return "Hello, World!";
     }
